e612ca22ebe4b5719133b1f4bb58814ca2145a31
bm10321-eng <bm10321@gmail.com>
2026-08-12 11:29:51 +0900

    앱 캐시 갱신

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='review-helper-v5';const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg','./app-icon-user.png','./forest-banner-user.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = 'review-helper-v6';
+const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest', './icon.svg', './app-icon-user.png', './forest-banner-user.png'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))));
