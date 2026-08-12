a13c3cbfe62321818df4428b917f9ac27c076077
bm10321-eng <bm10321@gmail.com>
2026-08-12 12:44:51 +0900

    Update cache version from v6 to v7OCR 업데이트 캐시 갱신

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'review-helper-v6';
+const CACHE = 'review-helper-v7';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest', './icon.svg', './app-icon-user.png', './forest-banner-user.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
